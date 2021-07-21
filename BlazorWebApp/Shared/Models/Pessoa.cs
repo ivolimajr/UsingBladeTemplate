9fe89321ba92601701157b1a19f07308ff79534b
Ivo Lima Jr <ivo.sl.jr@gmail.com>
2021-07-20 23:50:55 -0300

    V1 do sistema

---
+++ BlazorWebApp/Shared/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWebApp.Shared.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Peso { get; set; }
+    }
+}
